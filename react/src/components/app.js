8f7e66dec45f4992fec2b2d34a41db3a9bed2d33
fercc1097 <fercc1097@gmail.com>
2020-09-06 03:31:03 -0500

    usign hooks with react pd: webpack don't require a loader for json files

--- react/src/components/app.js
+++ react/src/components/app.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader';
+
+console.log(data);  
 
 function App(){
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
     return (
-        <p>Que linda app hecha en React.js</p>
+        <div>
+            <p>Que linda app hecha en React.js</p>
+            <ul>
+                {
+                    loaderList.map(item => <Loader {...item} key={item.id} />)
+                    
+                }
+            </ul>
+            <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+        </div>
     )
 }
 
