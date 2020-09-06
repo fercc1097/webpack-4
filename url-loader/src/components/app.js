a892baa589bf957ed289a3cd0562eb48eae4913f
fercc1097 <fercc1097@gmail.com>
2020-09-06 04:00:14 -0500

    converting small files to base64 with webpack

---
+++ url-loader/src/components/app.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader';
+import logo from '../images/platzi.png';
+import video from '../video/que-es-core.mp4';
+
+console.log(data);  
+
+function App(){
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
+    return (
+        <div>
+            <p>Que linda app hecha en React.js</p>
+            <p>
+                <video src = {video} width = {360} controls poster={logo}></video>
+                <img id="logo-platzi" src={logo}>
+                </img>
+            </p>
+            <p id="pWithFont">Hola Mundo</p>
+            <ul>
+                {
+                    loaderList.map(item => <Loader {...item} key={item.id} />)
+                    
+                }
+            </ul>
+            <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+        </div>
+    )
+}
+
+export default App;
